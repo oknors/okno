1d1d31ce068ad4bcaa216d390f5282eb98cc244f
marcetin <marcetin@gmail.com>
2021-09-29 17:30:01 +0200

    da

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        filename: env('DATABASE_FILENAME', '/var/db/sqlite/okno.db'),
       },
       options: {
         useNullAsDefault: true,
