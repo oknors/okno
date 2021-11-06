726ded1caf87d6d2089c4e926047d80006509ff8
marcetin <marcetin@gmail.com>
2021-11-06 02:06:54 +0100

    da

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 11111),
+  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6788c2dd5525743f1b81012a16638bba'),
